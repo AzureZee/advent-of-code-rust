5f29d05b3c806a62333a68e331da7861a894fee5
AzureZee <azureze@qq.com>
2025-12-24 00:41:03 +0800

    part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,10 +1,30 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let list = input
+        .lines()
+        .map(|line| line.parse::<u64>().unwrap())
+        .collect::<Vec<_>>();
+    let end = list.len() - 1;
+    let mut res = 0;
+    for (i, a) in list.iter().enumerate() {
+        let j = if i + 1 < end {
+            i + 1
+        } else {
+            break;
+        };
+        for b in &list[j..end] {
+            let sum = a + b;
+            if sum == 2020 {
+                res = a * b;
+                break;
+            }
+        }
+    }
+    Some(res)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+pub fn part_two(_input: &str) -> Option<u64> {
     None
 }
 
@@ -15,7 +35,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(514579));
     }
 
     #[test]
